966f041c4e8a2f5adda76629b5bf2861012aeece
Emanuel Bejenariu <132564222+bejenariu1996@users.noreply.github.com>
2023-05-29 14:17:34 +0200

    Update navigation.component.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from 'react';
-
+import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
@@ -41,7 +41,7 @@ const Navigation = () => {
         </NavLinks>
         {isCartOpen && <CartDropdown />}
       </NavigationContainer>
-    import { Outlet } from 'react-router-dom';
+      <Outlet />
     </Fragment>
   );
 };
